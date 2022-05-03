0ae9ef8c10d9b19f2640129a67a0c6bb63fa8171
Marcel Ardelean <102534901+ardeleanmarcel@users.noreply.github.com>
2022-05-03 18:59:58 +0300

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -16,36 +16,35 @@ function xOr0(cellNumber) {
   winningMessage();
 }
 
-const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
-let gameStatus = false;
-let playersChoices = "";
-
 function checkingTheWinner() {
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  let winningPlayer = "";
   for (let i = 0; i <= 7; ++i) {
-    playersChoices = winningConditions[i];
-    let condition1 = gameTable[playersChoices[0]];
-    let condition2 = gameTable[playersChoices[1]];
-    let condition3 = gameTable[playersChoices[2]];
+    const playersChoices = winningConditions[i];
+    const condition1 = gameTable[playersChoices[0]];
+    const condition2 = gameTable[playersChoices[1]];
+    const condition3 = gameTable[playersChoices[2]];
     if (condition1 === condition2 && condition2 === condition3 && condition2 !== "") {
-      gameStatus = true;
+      winningPlayer = condition1;
       break;
     }
   }
+  return winningPlayer;
 }
 
 function winningMessage() {
-  if (gameStatus) {
-  	resultMessage.innerHTML = `Player ${gameTable[playersChoices[0]]} win! Congratulations!`;
-  } else if (!(gameTable.includes(""))) {
+  if (checkingTheWinner() !== "") {
+  	resultMessage.innerHTML = `Player "${checkingTheWinner()}" win! Congratulations!`;
+   } else if (!(gameTable.includes(""))) {
   	resultMessage.innerHTML = "It is a draw!";
   }
 }
